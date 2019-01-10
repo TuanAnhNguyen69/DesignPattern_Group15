98ff178d787e8e16aceac9e403a2cab02024322c
NguyenTuanAnh69 <razordmonkey@live.com>
2019-01-11 04:37:01 +0700

    Add mediator, memento

---
+++ Mediator/Mediator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+
+    interface IChatRoomMediator
+    {
+        void SendMessage(User user, String message);
+    }
+
+    class ChatRoom : IChatRoomMediator
+    {
+        public void SendMessage(User user, string message)
+        {
+            Console.WriteLine(user.userName + ": " + message);
+        }
+    }
+
+    class User
+    {
+        protected IChatRoomMediator chatRoom;
+        public String userName;
+
+    public User(IChatRoomMediator chatRoom, String userName)
+        {
+            this.userName = userName;
+            this.chatRoom = chatRoom;
+        }
+
+        public void Send(String message)
+        {
+            chatRoom.SendMessage(this, message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChatRoom chatRoom = new ChatRoom();
+            User userA = new User(chatRoom, "A");
+            User userB = new User(chatRoom, "B");
+
+            userA.Send("Hello!");
+            userB.Send("Hi!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Memento/Memento/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class Program
+    {
+        class Storage // Memento
+        {
+            public Storage(String content)
+            {
+                this.content = content;
+            }
+
+            public String content { get; set; }
+        }
+
+        class Editor //Originator
+        {
+            public String content { get; set; }
+
+            public void Type(String content)
+            {
+                this.content = content;
+            }
+
+            public void UnDo(Storage storage)
+            {
+                this.content = storage.content;
+            }
+
+            public Storage Save()
+            {
+                return new Storage(content);
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Editor editor = new Editor();
+            Storage storage = new Storage("");
+
+            editor.Type("Abcd");
+            Console.WriteLine(editor.content);
+            storage = editor.Save();
+            Console.WriteLine("Saved");
+
+            editor.Type("ababababa");
+            Console.WriteLine(editor.content);
+            editor.UnDo(storage);
+            Console.WriteLine("UnDo");
+
+            Console.WriteLine(editor.content);
+            Console.ReadLine();
+        }
+    }
+}
