9a829723e6415f82da7da213502c85459bbe8513
ochutgio <ochutgio@gmail.com>
2019-01-10 15:51:10 +0700

    update_brigde

--- Demo_BridgePattern/BridgePattern/Program.cs
+++ Demo_BridgePattern/BridgePattern/Program.cs
@@ -12,8 +12,9 @@ namespace BridgePattern
         {
             IMessageSender email = new EmailSender();           
             IMessageSender sms = new SMSSender();
+            Message message;
 
-            Message message = new SystemMessage();
+            message = new SystemMessage();
             message.Subject = "Test Message";
             message.Body = "Hi, This is a Test Message";
 
@@ -23,14 +24,17 @@ namespace BridgePattern
             message.MessageSender = sms;
             message.Send();
 
-            //Message usermsg = new UserMessage();
-            //usermsg.Subject = "Test Message";
-            //usermsg.Body = "Hi, This is a Test Message";          
 
-            //usermsg.MessageSender = email;
-            //usermsg.Send();
-            //usermsg.MessageSender = sms;          
-            //usermsg.Send();
+            UserMessage usermsg = new UserMessage();
+            usermsg.Subject = "Test Message";
+            usermsg.Body = "Hi, This is a Test Message";
+            usermsg.UserComments = "attach a comment";
+
+            message = usermsg;
+            message.MessageSender = email;
+            message.Send();
+            message.MessageSender = sms;
+            message.Send();
 
             Console.ReadKey();
         }
