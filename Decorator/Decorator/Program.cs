67255d91b4a22115467ea6d32f64deb7e97bc5a9
NguyenTuanAnh69 <razordmonkey@live.com>
2019-01-11 15:19:29 +0700

    Update Decorator

--- Decorator/Decorator/Program.cs
+++ Decorator/Decorator/Program.cs
@@ -6,173 +6,84 @@ using System.Threading.Tasks;
 
 namespace Decorator
 {
+
+    interface Beverage
+    {
+        String GetIngredient();
+    }
+
+    class Tea : Beverage
+    {
+        public String ingredient;
+
+        public Tea()
+        {
+            this.ingredient = "Tea, hot water";
+        }
+
+        public String GetIngredient()
+        {
+            return this.ingredient;
+        }
+    }
+
+    class MachiatoTea : Beverage
+    {
+        Beverage tea;
+
+        public MachiatoTea(Beverage tea)
+        {
+            this.tea = tea;
+        }
+
+        public String GetIngredient()
+        {
+            return tea.GetIngredient() + ", machiato";
+
+        }
+    }
+
+    class ColdTea : Beverage
+    {
+        Beverage tea;
+
+        public ColdTea(Beverage tea)
+        {
+            this.tea = tea;
+        }
+
+        public String GetIngredient()
+        {
+            return tea.GetIngredient() + ", Ice";
+
+        }
+    }
+
+
     class Program
     {
         static void Main()
         {
-            // Create milktea
+            Tea tea = new Tea();
+            Console.WriteLine("TEA");
+            Console.WriteLine(tea.GetIngredient());
 
-            MilkTea milkTea = new MilkTea("BlackBorn", new List<string> {"Milk", "Tea"}, "Jelly");
-            milkTea.ShowIngredient();
+    
+            MachiatoTea machiato = new MachiatoTea(tea);
+            Console.WriteLine("MachiatoTEA");
+            Console.WriteLine(machiato.GetIngredient());
 
-            // Create machiato
+            ColdTea coldTea = new ColdTea(tea);
+            Console.WriteLine("ColdTEA");
+            Console.WriteLine(coldTea.GetIngredient());
 
-            Machiato machiato = new Machiato("Macha", new List<string> { "Macha", "BlackTea" });
-            machiato.ShowIngredient();
+            ColdTea coldMachiatoTea = new ColdTea(machiato);
+            Console.WriteLine("ColdMachiatoTEA");
+            Console.WriteLine(coldMachiatoTea.GetIngredient());
 
-            Custommer custommer = new Custommer(machiato);
-            custommer.OrderItem("Customer 1");
-            custommer.OrderItem("Customer 2");
-
-            custommer.ShowIngredient();
 
             // Wait for user
-
             Console.ReadKey();
-        }
-    }
-
-    /// <summary>
-
-    /// The 'Component' abstract class
-
-    /// </summary>
-
-    abstract class Beverage
-    {
-        protected string name;
-        protected List<string> _ingredients;
-
-        // Property
-        public List<string> Ingredients
-        {
-            get { return _ingredients; }
-            set { _ingredients = value; }
-        }
-
-        public abstract void ShowIngredient();
-    }
-
-    /// <summary>
-
-    /// The 'ConcreteComponent' class
-
-    /// </summary>
-
-    class MilkTea : Beverage
-    {
-        private string topping;
-
-        // Constructor
-
-        public MilkTea(string name, List<string> ingredients, string topping)
-        {
-            this.name = name;
-            this.Ingredients = ingredients;
-            this.topping = topping;
-        }
-
-        public override void ShowIngredient()
-        {
-            Console.WriteLine("\nMilktea ------ ");
-            Console.WriteLine("Name: {0}", name);
-            Console.WriteLine("Toping: {0}", topping);
-            Console.WriteLine("Ingredients:");
-            foreach (string ingredient in Ingredients)
-            {
-                Console.WriteLine("\n{0}", ingredient);
-            }
-        }
-    }
-
-    /// <summary>
-
-    /// The 'ConcreteComponent' class
-
-    /// </summary>
-
-    class Machiato : Beverage
-    {
-        // Constructor
-
-        public Machiato(string name, List<string> ingredients)
-        {
-            this.name = name;
-            this.Ingredients = ingredients;
-        }
-
-        public override void ShowIngredient()
-        {
-            Console.WriteLine("\nMachiato ------ ");
-            Console.WriteLine("Name: {0}", name);
-            Console.WriteLine("Ingredients:");
-            foreach (string ingredient in Ingredients)
-            {
-                Console.WriteLine("\n{0}", ingredient);
-            }
-        }
-    }
-
-    /// <summary>
-
-    /// The 'Decorator' abstract class
-
-    /// </summary>
-
-    abstract class Decorator : Beverage
-
-    {
-        protected Beverage beverage;
-
-        // Constructor
-
-        public Decorator(Beverage libraryItem)
-        {
-            this.beverage = libraryItem;
-        }
-
-        public override void ShowIngredient()
-        {
-            beverage.ShowIngredient();
-        }
-    }
-
-    /// <summary>
-
-    /// The 'ConcreteDecorator' class
-
-    /// </summary>
-
-    class Custommer : Decorator
-
-    {
-        protected List<string> custommer = new List<string>();
-
-        // Constructor
-
-        public Custommer(Beverage beverage)
-            : base(beverage)
-        {
-        }
-
-        public void OrderItem(string name)
-        {
-            custommer.Add(name);
-        }
-
-        public void ReturnItem(string name)
-        {
-            custommer.Remove(name);
-        }
-
-        public override void ShowIngredient()
-        {
-            base.ShowIngredient();
-
-            foreach (string borrower in custommer)
-            {
-                Console.WriteLine(" borrower: " + borrower);
-            }
         }
     }
 }
